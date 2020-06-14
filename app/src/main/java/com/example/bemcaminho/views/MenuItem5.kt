a0fe58e2ff08069578c97f7a2aaf053d23242c46
igorjunio2015 <igorjunio2015@gmail.com>
2020-06-14 19:27:45 -0300

    Last update

--- app/src/main/java/com/example/bemcaminho/views/MenuItem5.kt
+++ app/src/main/java/com/example/bemcaminho/views/MenuItem5.kt
@@ -1,8 +1,10 @@
 package com.example.bemcaminho.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import com.example.bemcaminho.HomePageActivity
 import com.example.bemcaminho.R
 
 class MenuItem5 : AppCompatActivity() {
@@ -12,5 +14,9 @@ class MenuItem5 : AppCompatActivity() {
         setContentView(R.layout.activity_menu_item5)
 
         val btnReturn = findViewById<ImageView>(R.id.btn_return)
+        btnReturn.setOnClickListener {
+            val homePage = Intent(this, HomePageActivity::class.java)
+            startActivity(homePage)
+        }
     }
 }
